79bd88c9a2aa86aba301dfa1a2ea38931cb2df71
Paula Rusti <rusti.paula78@gmail.com>
2022-12-12 20:09:42 +0200

    use messages only for the login events

--- CatanStrategyGame/src/main/java/org/cebp/Main.java
+++ CatanStrategyGame/src/main/java/org/cebp/Main.java
@@ -1,22 +1,41 @@
 package org.cebp;
 
-import org.cebp.client.Client;
+
 import org.cebp.model.Game;
 import org.cebp.model.Player;
+import org.cebp.rabbit.RabbitClient;
+import org.cebp.rabbit.RabbitMessage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeoutException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, TimeoutException, ClassNotFoundException {
+        RabbitClient rabbitClient = new RabbitClient();
+        rabbitClient.initializeConnection();
+
+
         ExecutorService service = Executors.newFixedThreadPool(3);
         ArrayList<Player> players = new ArrayList<>();
-        Player player1 = new Player("JohnDoe");
-        Player player2 = new Player("Batman");
+        Player player1 = new Player("JohnDoe", rabbitClient);   // generate random player method?
+        Player player2 = new Player("Batman", rabbitClient);
         players.add(player1);
         players.add(player2);
+
+        player1.sendLoginMessage();     // post login message to server queue
+        player2.sendLoginMessage();
+
+        RabbitMessage loginUser1 = (RabbitMessage) rabbitClient.consumeSync();  // read message from queue
+        RabbitMessage loginUser2 = (RabbitMessage) rabbitClient.consumeSync();
+
+        String player_name_1 = loginUser1.getPlayerName();  // get player name from message
+        String player_name_2 = loginUser2.getPlayerName();
+
+
 
         // server
         Game game = new Game(players);
@@ -25,10 +44,10 @@ public class Main {
         game.initializeCommonResources();
 
         // 2 players
-        Client client1 = new Client("JohnDoe");
-        Client client2 = new Client("Batman");
-        client1.createClient();
-        client2.createClient();
+//        Client client1 = new Client("JohnDoe");
+//        Client client2 = new Client("Batman");
+//        client1.createClient();
+//        client2.createClient();
 
         service.execute(game);
 
